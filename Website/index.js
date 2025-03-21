40dc42f1630a4e4f6de59a6a70e07def014b0142
Eduard Pavel <82057743+EduardPavelMarangoci@users.noreply.github.com>
2025-03-21 17:36:40 +0000

    finalized home page

--- Website/index.js
+++ Website/index.js
@@ -1,1 +1,20 @@
-// javascript
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".nav-mobile-toggle");
+
+
+
+// when someone clicks the hamburger button
+// if the nav is closed, open it
+// if the nav is open, close it
+
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    console.log(navToggle);
+    if(visibility === "false"){
+        nav.setAttribute("Data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("Data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
